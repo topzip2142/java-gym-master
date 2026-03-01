e333ae9a6c8883d3f85d93e60c9c2419b2cedb62
topzip <topzip2142@yandex.ru>
2026-03-01 17:02:34 +0300

    Доработать класс расписания

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,41 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private final Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable;
+
+    public Timetable() {
+        timetable = new HashMap<>();
+        for (DayOfWeek day : DayOfWeek.values()) {
+            timetable.put(day, new TreeMap<>());
+        }
+    }
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+        DayOfWeek day = trainingSession.getDayOfWeek();
+        TimeOfDay time = trainingSession.getTimeOfDay();
+        TreeMap<TimeOfDay, List<TrainingSession>> sessionsForDay = timetable.get(day);
+        List<TrainingSession> sessionsAtTime = sessionsForDay.get(time);
+
+        if (sessionsAtTime == null) {
+            sessionsAtTime = new ArrayList<>();
+            sessionsForDay.put(time, sessionsAtTime);
+        }
+        sessionsAtTime.add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public TreeMap<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        System.out.println(dayOfWeek + ": ");
+        return timetable.get(dayOfWeek);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        TreeMap<TimeOfDay, List<TrainingSession>> sessionsForDay = timetable.get(dayOfWeek);
+        System.out.println(dayOfWeek + " " + timeOfDay + " :");
+        if (sessionsForDay == null) {
+            return new ArrayList<>();
+        }
+        List<TrainingSession> sessions = sessionsForDay.get(timeOfDay);
+
+        return sessions;
     }
 }
